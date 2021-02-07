70897af80f2f8e890d913beaaf4d514e4e031664
CheriseCodes <cherise.bry@gmail.com>
2021-02-07 07:22:06 -0500

    Set up expenses and home page

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,27 @@
 # flask_web/app.py
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+
 app = Flask(__name__)
+app.secret_key = 'my_key'
+app.debug = True
+
+username = ""
 
 @app.route('/')
+def home():
+    return render_template('home.html')
 
-def hello_world():
-    return 'Here is a flask app'
+@app.route('/expenses')
+def expenses():
+    return render_template('expenses.html', username=username)
 
+@app.route("/setname", methods=["POST"])
+def setname():
+    global username
+    username = request.form["username"]
+    return redirect('/expenses')
+    return expenses()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
